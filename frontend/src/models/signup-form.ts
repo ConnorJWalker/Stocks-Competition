4d8b8c3f10a39a70c99cc33e972fc1a013b52615
Connor Walker <connorjwalker32@hotmail.co.uk>
2022-12-07 21:26:43 +0000

    added colour picker functionality

--- frontend/src/models/signup-form.ts
+++ frontend/src/models/signup-form.ts
@@ -1,10 +1,29 @@
-export default class SignupForm {
-    profilePicture: string = ''
-    displayName: string = ''
-    discordUsername: string = ''
-    displayColour: string = ''
-    password: string = ''
-    passwordConfirm: string = ''
+export interface ISignupForm {
+    profilePicture: string
+    displayName: string
+    discordUsername: string
+    displayColour: string
+    password: string
+    passwordConfirm: string
+    freetradeCookie: string 
+}
 
-    freetradeCookie: string = ''
+export default function createSignupForm(): ISignupForm {
+    let displayColour = '#'
+    
+    // Generate initial random colour
+    const validCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f']
+    for (let i = 0; i < 6; i++) {
+        displayColour += validCharacters[Math.floor(Math.random() * validCharacters.length)]
+    }
+
+    return {
+        profilePicture: '',
+        displayName: '',
+        discordUsername: '',
+        displayColour,
+        password: '',
+        passwordConfirm: '',
+        freetradeCookie: ''
+    }
 }
--- frontend/src/views/authentication/forms/signup.tsx
+++ frontend/src/views/authentication/forms/signup.tsx
@@ -1,70 +1,72 @@
 import { useState } from 'react'
-import SignupForm from '../../../models/signup-form'
+import createSignupForm from '../../../models/signup-form'
 
 export default function Signup(props: any) {
-    const signupFrom = useState(new SignupForm())[0]
+    const [signupForm, setSignupForm] = useState(createSignupForm())
     const [page, setPage] = useState(1)
 
-    return page === 1 
-        ? renderSignupForm(signupFrom, props.SetShowSignup, setPage) 
-        : renderCookieForm(signupFrom, props.SetShowSignup, setPage)
-}
-
-function renderSignupForm(signupFrom: SignupForm, setShowSignup: SetState<boolean>, setPage: SetState<number>) {
-    return (
-        <div className="form-container">
-            <div className="form-group">
-                { renderProfilePicture(signupFrom) }
-                <div className="text-input-group">
-                    <input placeholder="Display Name" type="text" />
-                    <input placeholder="Discord Username" type="text" />
+    function renderSignupForm() {
+        return (
+            <div className="form-container">
+                <div className="form-group">
+                    { renderProfilePicture() }
+                    <div className="text-input-group">
+                        <input placeholder="Display Name" type="text" />
+                        <input placeholder="Discord Username" type="text" />
+                    </div>
                 </div>
+                <div className="form-group">
+                    <div className="text-input-group">
+                        <input placeholder="Password" type="password" />
+                        <input placeholder="Confirm Password" type="password" />
+                    </div>
+                    <label className='colour-picker' style={{ backgroundColor: signupForm.displayColour }}>
+                        <small>Colour</small>
+                        <input 
+                            type="color" 
+                            value={signupForm.displayColour}
+                            onChange={e => setSignupForm({ ...signupForm, displayColour: e.target.value })}
+                            hidden />
+                    </label>
+                </div>
+    
+                <footer>
+                    <a href="#"  onClick={() => props.SetShowSignup(false)}>Log In</a>
+                    <button className="btn-pink" onClick={() => setPage(2)}>Next</button>
+                </footer>
             </div>
-            <div className="form-group">
-                <div className="text-input-group">
-                    <input placeholder="Password" type="password" />
-                    <input placeholder="Confirm Password" type="password" />
-                </div>
-                <label className='colour-picker'>
-                    <small>Colour</small>
-                    <input type="color" hidden />
-                </label>
+        )
+    }
+
+    function renderProfilePicture() {
+        return signupForm.profilePicture !== ''
+            ? <img className="profile-picture" src={signupForm.profilePicture} />
+            : <div className="profile-picture"><p>Picture</p></div> 
+    }
+
+    function renderCookieForm() {
+        return (
+            <div className="form-container">
+                <h1>Freetrade Authentication Cookie</h1>
+                <ul>
+                    <li>Log into the Freetrade <a href="https://web.freetrade.io/login" target="_blank">web interface</a></li>
+                    <li>Open your browsers dev tools and find cookies</li>
+                    <li>Copy and paste the value of <code>ft_web_session</code> into the text area</li>
+                </ul>
+    
+                <textarea onChange={e => signupForm.freetradeCookie = e.target.value}></textarea>
+    
+                <footer>
+                    <a href="#"  onClick={() => props.SetShowSignup(false)}>Log In</a>
+                    
+                    <div>
+                        <button onClick={() => setPage(1)}>Back</button>
+                        <button className="btn-pink">Sign Up</button>
+                    </div>
+                </footer>
             </div>
+        )
+    }
 
-            <footer>
-                <a href="#"  onClick={() => setShowSignup(false)}>Log In</a>
-                <button className="btn-pink" onClick={() => setPage(2)}>Next</button>
-            </footer>
-        </div>
-    )
-}
-
-function renderProfilePicture(signupFrom: SignupForm) {
-    return signupFrom.profilePicture !== ''
-        ? <img className="profile-picture" src={signupFrom.profilePicture} />
-        : <div className="profile-picture"><p>Picture</p></div> 
-}
-
-function renderCookieForm(signupForm: SignupForm, setShowSignup: SetState<boolean>, setPage: SetState<number>) {
-    return (
-        <div className="form-container">
-            <h1>Freetrade Authentication Cookie</h1>
-            <ul>
-                <li>Log into the Freetrade <a href="https://web.freetrade.io/login" target="_blank">web interface</a></li>
-                <li>Open your browsers dev tools and find cookies</li>
-                <li>Copy and paste the value of <code>ft_web_session</code> into the text area</li>
-            </ul>
-
-            <textarea onChange={e => signupForm.freetradeCookie = e.target.value}></textarea>
-
-            <footer>
-                <a href="#"  onClick={() => setShowSignup(false)}>Log In</a>
-                
-                <div>
-                    <button onClick={() => setPage(1)}>Back</button>
-                    <button className="btn-pink">Sign Up</button>
-                </div>
-            </footer>
-        </div>
-    )
+    return page === 1 ? renderSignupForm() : renderCookieForm()
 }
